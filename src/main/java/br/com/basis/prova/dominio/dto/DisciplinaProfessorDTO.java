8d9f5a6d269d0a998867e7de5e9987ddf8433960
Alan Oliveira <alanjhone@gmail.com>
2020-08-19 09:05:31 -0300

    DisciplinaProfessoDTO

---
+++ src/main/java/br/com/basis/prova/dominio/dto/DisciplinaProfessorDTO.java
@@ -0,0 +1,14 @@
+package br.com.basis.prova.dominio.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class DisciplinaProfessorDTO {
+    private String nome;
+}
