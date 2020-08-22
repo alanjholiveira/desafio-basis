5060b1dc735b2971ac19c1e35fd8322d101f4493
Alan Oliveira <alanjhone@gmail.com>
2020-08-22 09:33:35 -0300

    implementando corsConfig

--- src/main/java/br/com/basis/prova/ProvaApplication.java
+++ src/main/java/br/com/basis/prova/ProvaApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableConfigurationProperties({LiquibaseProperties.class})
@@ -14,16 +11,6 @@ public class ProvaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProvaApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
-			}
-		};
 	}
 
 }
